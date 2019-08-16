93f300b73ee6a20b08c01fe8fe4279e800404652
谢小东 <xiexiaodong@126.com>
2019-08-16 08:52:53 +0800

    提交最新

---
+++ utils/table.js
@@ -0,0 +1,55 @@
+const tableTemplate =`<table>
+<tr class="title-tr">
+    <td>Name</td>
+    <td>City</td>
+    <td>Age</td>
+</tr>
+<tr>
+    <td>Tom</td>
+    <td>
+        <tr class="clear-border border-btm">
+          <td  class="clear-border">111111111111111111111111111111111111111111</td>
+        </tr>
+        <tr class="clear-border border-btm">
+          <td  class="clear-border">11</td>
+        </tr>
+        <tr class="clear-border border-btm">
+          <td  class="clear-border">11</td>
+        </tr>
+    </td>
+
+    <td>
+        <tr class="clear-border border-btm">
+          <td  class="clear-border">11</td>
+        </tr>
+        <tr class="clear-border border-btm">
+          <td  class="clear-border">11</td>
+        </tr>
+        <tr class="clear-border border-btm">
+          <td  class="clear-border">11</td>
+        </tr>
+    </td>
+    
+</tr>
+<tr>
+    <td>Jack</td>
+    <td>北京</td>
+    <td></td>
+ </tr>
+
+ <tr class="title-tr">
+    <td><div class="table-star-header"><div>主检建议</div><div>***</div></div></td>
+    
+ </tr>
+
+  <tr>
+    <td>xdsafsadf</td>
+  </tr>
+
+
+</table>`;
+
+
+const tabledata = [[{ "text": "视力眼压", "rowClassId": "" }, { "rowItems": [[{ "itemClassId": "", "text": "视力(右)" }, { "itemClassId": "", "text": "0.2" }, { "itemClassId": "" }, { "itemClassId": "", "text": "↓" }, { "itemClassId": "", "text": "1-2" }], [{ "itemClassId": "", "text": "视力(左)" }, { "itemClassId": "", "text": "0.2" }, { "itemClassId": "" }, { "itemClassId": "", "text": "↓" }, { "itemClassId": "", "text": "1-2" }], [{ "itemClassId": "", "text": "矫正视力(左)" }, { "itemClassId": "", "text": "0.9" }, { "itemClassId": "" }, { "itemClassId": "", "text": "↓" }, { "itemClassId": "", "text": "1-2" }], [{ "itemClassId": "", "text": "散光" }, { "itemClassId": "", "text": "散光" }, { "itemClassId": "" }, { "itemClassId": "" }, { "itemClassId": "" }]], "rowClassId": "" }], [{ "text": "心电图", "rowClassId": "" }, { "rowItems": [[{ "itemClassId": "", "text": "心电图" }, { "itemClassId": "", "text": "窦性心律不齐" }, { "itemClassId": "" }, { "itemClassId": "" }, { "itemClassId": "" }]], "rowClassId": "" }], [{ "text": "口腔检查", "rowClassId": "" }, { "rowItems": [[{ "itemClassId": "", "text": "牙齿" }, { "itemClassId": "", "text": "龋齿左上7" }, { "itemClassId": "" }, { "itemClassId": "" }, { "itemClassId": "" }], [{ "itemClassId": "", "text": "牙龈" }, { "itemClassId": "", "text": "牙龈炎；牙结石I度" }, { "itemClassId": "" }, { "itemClassId": "" }, { "itemClassId": "" }]], "rowClassId": "" }], [{ "text": "尿酸检查", "rowClassId": "" }, { "rowItems": [[{ "itemClassId": "", "text": "尿酸" }, { "itemClassId": "", "text": "475" }, { "itemClassId": "", "text": "μmol/L" }, { "itemClassId": "", "text": "↑" }, { "itemClassId": "", "text": "208-428" }]], "rowClassId": "" }], [{ "text": "耳鼻喉", "rowClassId": "" }, { "rowItems": [[{ "itemClassId": "", "text": "口咽" }, { "itemClassId": "", "text": "急性咽炎" }, { "itemClassId": "" }, { "itemClassId": "" }, { "itemClassId": "" }], [{ "itemClassId": "", "text": "喉" }, { "itemClassId": "", "text": "急性喉炎" }, { "itemClassId": "" }, { "itemClassId": "" }, { "itemClassId": "" }]], "rowClassId": "" }], [{ "text": "血脂检查", "rowClassId": "" }, { "rowItems": [[{ "itemClassId": "", "text": "高密度脂蛋白胆固醇" }, { "itemClassId": "", "text": "0.83" }, { "itemClassId": "", "text": "mmol/L" }, { "itemClassId": "", "text": "↓" }, { "itemClassId": "", "text": "1.04-1.56" }], [{ "itemClassId": "", "text": "低密度脂蛋白胆固醇" }, { "itemClassId": "", "text": "3.51" }, { "itemClassId": "", "text": "mmol/L" }, { "itemClassId": "", "text": "↑" }, { "itemClassId": "", "text": "<3.37" }]], "rowClassId": "" }]];
+
+export default tableTemplate
